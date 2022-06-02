d08e1cfe1bc743d7f624d395a81a3900ffe5b693
vivi.vega <viviana.vega@accedo.tv>
2022-06-02 17:54:11 +0200

    init

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="app">
+      <h1>
+        Welcome to React App thats build using Webpack and Babel separately
+      </h1>
+      <p>
+        Here you get a
+        <a href="https://blog.bitsrc.io/create-react-app-without-create-react-app-b0a5806a92">
+          blog where you can learn about you have to do to build your own
+          react project without help{" "}
+        </a>
+      </p>
+    </div>
+  );
+};
+
+export default App;
